c718c606094f37edd0130f49d1f69992e116972c
ninemine <1371605831@qq.com>
2026-06-21 16:00:51 +0800

    [fix]修复菜单按钮样式丢失

--- origin_agent/frontend/src/main.tsx
+++ origin_agent/frontend/src/main.tsx
@@ -14,6 +14,7 @@ import "./styles/panels.css";
 import "./styles/drawer.css";
 import "./styles/input.css";
 import "./styles/tooltip.css";
+import "./styles/context-menu.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
